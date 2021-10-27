fa0d0086310083f8a2a0e8fcf8ccc82e3a17db57
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-10-27 09:59:55 -0300

    feat(global): :construction: create uniqueId valueobject

--- lib/domain/global/core/value_objects.dart
+++ lib/domain/global/core/value_objects.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 import 'package:versus_deum_app/domain/global/core/errors.dart';
 import 'package:versus_deum_app/domain/global/core/failures.dart';
 
@@ -24,4 +25,22 @@ abstract class ValueObject<T> {
 
   @override
   String toString() => 'Value(value: $value)';
+}
+
+class UniqueId extends ValueObject {
+  const UniqueId._(this.value);
+
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(uniqueId),
+    );
+  }
+
+  @override
+  final Either<ValueFailure, dynamic> value;
 }
