8e4f5d303df36380c116e6f62c14df83d9fefea9
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-10-27 10:23:21 -0300

    feat(auth): :construction: create valueObjects

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,26 @@
+import 'package:versus_deum_app/domain/global/core/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:versus_deum_app/domain/global/core/value_objects.dart';
+import 'package:versus_deum_app/domain/global/core/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(validateEmailAddress(input));
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(validatePassword(input));
+  }
+
+  const Password._(this.value);
+}
