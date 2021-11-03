07ffb559f03414fd226d5fe3bdafa19ae35345f9
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-11-03 10:56:40 -0300

    feat(auth): :construction: create method uid.failureOrUnit

---
+++ lib/domain/auth/auth_user.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:versus_deum_app/domain/global/value_failure.dart';
+import 'package:versus_deum_app/domain/global/value_objects.dart';
+
+part 'auth_user.freezed.dart';
+
+@freezed
+class AuthUser with _$AuthUser {
+  const factory AuthUser({
+    required UniqueID uid,
+    required Email email,
+  }) = _AuthUser;
+
+  const AuthUser._();
+
+  Option<ValueFailure<dynamic>> get failureOption => uid.failureOrUnit
+      .andThen(email.failureOrUnit)
+      .fold((f) => some(f), (r) => none());
+}
---
+++ lib/domain/auth/auth_user.freezed.dart
@@ -0,0 +1,155 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'auth_user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$AuthUserTearOff {
+  const _$AuthUserTearOff();
+
+  _AuthUser call({required UniqueID uid, required Email email}) {
+    return _AuthUser(
+      uid: uid,
+      email: email,
+    );
+  }
+}
+
+/// @nodoc
+const $AuthUser = _$AuthUserTearOff();
+
+/// @nodoc
+mixin _$AuthUser {
+  UniqueID get uid => throw _privateConstructorUsedError;
+  Email get email => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AuthUserCopyWith<AuthUser> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AuthUserCopyWith<$Res> {
+  factory $AuthUserCopyWith(AuthUser value, $Res Function(AuthUser) then) =
+      _$AuthUserCopyWithImpl<$Res>;
+  $Res call({UniqueID uid, Email email});
+}
+
+/// @nodoc
+class _$AuthUserCopyWithImpl<$Res> implements $AuthUserCopyWith<$Res> {
+  _$AuthUserCopyWithImpl(this._value, this._then);
+
+  final AuthUser _value;
+  // ignore: unused_field
+  final $Res Function(AuthUser) _then;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? email = freezed,
+  }) {
+    return _then(_value.copyWith(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as UniqueID,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as Email,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$AuthUserCopyWith<$Res> implements $AuthUserCopyWith<$Res> {
+  factory _$AuthUserCopyWith(_AuthUser value, $Res Function(_AuthUser) then) =
+      __$AuthUserCopyWithImpl<$Res>;
+  @override
+  $Res call({UniqueID uid, Email email});
+}
+
+/// @nodoc
+class __$AuthUserCopyWithImpl<$Res> extends _$AuthUserCopyWithImpl<$Res>
+    implements _$AuthUserCopyWith<$Res> {
+  __$AuthUserCopyWithImpl(_AuthUser _value, $Res Function(_AuthUser) _then)
+      : super(_value, (v) => _then(v as _AuthUser));
+
+  @override
+  _AuthUser get _value => super._value as _AuthUser;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? email = freezed,
+  }) {
+    return _then(_AuthUser(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as UniqueID,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as Email,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_AuthUser extends _AuthUser {
+  const _$_AuthUser({required this.uid, required this.email}) : super._();
+
+  @override
+  final UniqueID uid;
+  @override
+  final Email email;
+
+  @override
+  String toString() {
+    return 'AuthUser(uid: $uid, email: $email)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _AuthUser &&
+            (identical(other.uid, uid) || other.uid == uid) &&
+            (identical(other.email, email) || other.email == email));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, uid, email);
+
+  @JsonKey(ignore: true)
+  @override
+  _$AuthUserCopyWith<_AuthUser> get copyWith =>
+      __$AuthUserCopyWithImpl<_AuthUser>(this, _$identity);
+}
+
+abstract class _AuthUser extends AuthUser {
+  const factory _AuthUser({required UniqueID uid, required Email email}) =
+      _$_AuthUser;
+  const _AuthUser._() : super._();
+
+  @override
+  UniqueID get uid;
+  @override
+  Email get email;
+  @override
+  @JsonKey(ignore: true)
+  _$AuthUserCopyWith<_AuthUser> get copyWith =>
+      throw _privateConstructorUsedError;
+}
